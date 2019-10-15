51d4627b9317183c641dd216e8b67289e3c214a0
Manuel Ojeda <manuelojedasistemas@gmail.com>
2019-10-14 21:25:49 -0600

    Added Rick n Morty app with React

---
+++ public/react/rick-n-morty/precache-manifest.c61836ee9503c5b884de376c7bea3f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e36204b86cf2f127360dbfd23efef4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b75a060afab9ec6a4465",
+    "url": "./static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "c1cb14392460e0e86e99",
+    "url": "./static/css/main.ecbd693e.chunk.css"
+  },
+  {
+    "revision": "b75a060afab9ec6a4465",
+    "url": "./static/js/2.2af7f70f.chunk.js"
+  },
+  {
+    "revision": "c1cb14392460e0e86e99",
+    "url": "./static/js/main.02845cdb.chunk.js"
+  },
+  {
+    "revision": "1caf6c128a962e170025",
+    "url": "./static/js/runtime-main.38aaac3e.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "./static/media/9OiY-rmlogo.42d6efdb.png"
+  }
+]);
