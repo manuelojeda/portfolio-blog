98f6a570606211c6287dac2780a46c440004d288
Manuel Ojeda <manuelojedasistemas@gmail.com>
2020-08-02 19:27:35 -0600

    🛠 Hotfix - Adding margin to container at blogs catalog

--- resources/js/Public/components/Blog/Index.vue
+++ resources/js/Public/components/Blog/Index.vue
@@ -95,6 +95,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  @media (min-width: 768px) {
+    margin-bottom: 5rem;
+  }
+}
 .grid {
   display: grid;
   grid-template-columns: 1fr;
