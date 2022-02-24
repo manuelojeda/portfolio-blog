a49331dd79c07b8671c484e5194b4d19c705a90e
Manuel Ojeda <manuelojedasistemas@gmail.com>
2022-02-24 11:59:21 -0700

    Header fix

--- resources/js/Public/Components/PartialHeader.vue
+++ resources/js/Public/Components/PartialHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class=" border-gray-200 px-2 sm:px-4 py-2.5 rounded mt-8">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="#" class="flex">
+      <a href="/" class="flex">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-emerald-500">Manuel Ojeda Blog</span>
       </a>
       <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
